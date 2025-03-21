9ef7240aa8fcef4a5566e3c621fb11e2b430d5c2
TameableDEV <littl@TameablesLAPTOP.home>
2025-03-20 17:41:10 -0700

    Started Gamblers

---
+++ src/main/java/net/alzo/alzosmod/procedures/GamblersOriginMainProcedure.java
@@ -0,0 +1,9 @@
+package net.alzo.alzosmod.procedures;
+
+import net.alzo.alzosmod.AlzosModMod;
+
+public class GamblersOriginMainProcedure {
+	public static void execute() {
+		AlzosModMod.LOGGER.info("FUCKKKKKK AHOIHIODWYFOIHF");
+	}
+}
